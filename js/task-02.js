860ffd135a7b4879ff05a36ded4c018d65391447
AlexKarb <alex.workinfo97@gmail.com>
2021-10-21 00:28:18 +0300

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const ingredientList = document.querySelector('#ingredients');
+
+const createIngredients = array => array.reduce((ary, el) => {
+
+    const ingredientItem = document.createElement("li");
+   
+    ingredientItem.textContent = el;
+    ingredientItem.classList.add('item');
+    ary.push(ingredientItem);
+
+    return ary;
+    
+  }, [])
+
+
+
+
+ingredientList.append(...createIngredients(ingredients))
+
