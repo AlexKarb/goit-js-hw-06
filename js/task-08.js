7375e21c9c2766c95e5f31be9e64d4c530657587
AlexKarb <alex.workinfo97@gmail.com>
2021-10-21 19:02:57 +0300

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const formEl = document.querySelector(".login-form");
+
+
+
+formEl.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+
+    const { elements: { email, password } } = e.currentTarget;
+
+    if (!email.value || !password.value){
+      return  alert("Все поля должны быть заполнены!")
+    }
+
+    const userData = {
+        email: email.value,
+        password: password.value,
+    }
+
+    console.log(userData)
+    e.currentTarget.reset();
+    
+});
+
+
 
