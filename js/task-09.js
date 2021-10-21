c805397cf59a65c1db167bdea85b62514ffd581c
AlexKarb <alex.workinfo97@gmail.com>
2021-10-21 21:36:48 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const bntForChangeColor = document.querySelector(".change-color");
+const nameOfColor = document.querySelector(".color");
+
+
+
+bntForChangeColor.addEventListener('click', () => {
+
+  document.body.style.backgroundColor = getRandomHexColor();
+  nameOfColor.textContent = getRandomHexColor()
+
+})
+
