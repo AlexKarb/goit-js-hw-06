d5bb97931cc2a4ca8c25bd09eeb8904ec8245f7e
AlexKarb <alex.workinfo97@gmail.com>
2021-10-21 17:53:26 +0300

    add task 6

---
+++ js/task-06.js
@@ -0,0 +1,24 @@
+
+const inputEl = document.querySelector('#validation-input')
+const inputLengthOfSymbol = Number(inputEl.dataset.length);
+
+
+function addStyleValid() {
+     inputEl.classList.contains('invalid') ? inputEl.classList.replace( "invalid", "valid") : inputEl.classList.add("valid");
+}
+ 
+function addStyleInvalid() {
+    inputEl.classList.contains('valid') ? inputEl.classList.replace("valid", "invalid") : inputEl.classList.add("invalid");
+}
+
+
+
+inputEl.addEventListener('blur', () => {
+    
+    const usersLengthOfSymbols = inputEl.value.length;
+    
+    return usersLengthOfSymbols === inputLengthOfSymbol ? addStyleValid() : addStyleInvalid();
+    })
+
+
+
