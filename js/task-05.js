b956ade214ec978962794052950952d0cf0ba9ee
AlexKarb <alex.workinfo97@gmail.com>
2021-10-22 16:08:48 +0300

    update 5 task

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,10 @@
 
-
-
 const ref = {
     input: document.querySelector('#name-input'),
     userName: document.querySelector("#name-output"),
 }
 
 
-ref.input.addEventListener('input', () => ref.userName.textContent = ref.input.value);
+ref.input.addEventListener('input', () => 
+  !ref.input.value ? ref.userName.textContent = "Anonymous" : ref.userName.textContent = ref.input.value
+ );
